3eacdc801eea268d05208492868ea626d6fccd9a
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-14 22:31:37 -0500

    añadimos, react-router, carpeta page, details y sus estilos. Tambien hicimos el llamado a la api de tmdb

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styles from "./App.module.css";
+// import { MoviesGrid } from "./components/MoviesGrid";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
+export function App() {
+  return (
+    <Router className="App">
+      <header>
+        <Link to="/">
+          <h1 className={styles.title}>Movies</h1>
+        </Link>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/movies/:movieId">
+            <MovieDetails />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+}
+
+// export default App;
