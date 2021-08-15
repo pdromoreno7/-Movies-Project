c3816256498dcf8ed24bd8e1519d3d69bcf7a264
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-14 22:52:40 -0500

    añadimos el llamado al movieId en details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,23 @@
-import movie from "./movie.json";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+// import movie from "./movie.json";
+import { get } from "../utils/httpClient";
 import styles from "./MovieDetails.module.css";
 
 export function MovieDetails() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+    });
+  }, [movieId]);
+
+  if (!movie) {
+    return null;
+  }
+
   const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
   return (
     <div className={styles.detailsContainer}>
