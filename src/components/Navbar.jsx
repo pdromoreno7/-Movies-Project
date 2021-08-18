5474c8effe96c501f7cacae98f7344927f266e6a
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-18 18:58:29 -0500

    añadimos posicion fixed al header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,28 +14,30 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          Movies <i className="fab fa-react"></i>
-        </h1>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a href={item.url} className={item.cName}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        {/* <Button>Donar</Button> */}
-      </nav>
+      <header className="l-header">
+        <nav className="NavbarItems">
+          <h1 className="navbar-logo">
+            Movies <i className="fab fa-react"></i>
+          </h1>
+          <div className="menu-icon" onClick={this.handleClick}>
+            <i
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+          <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a href={item.url} className={item.cName}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          {/* <Button>Donar</Button> */}
+        </nav>
+      </header>
     );
   }
 }
